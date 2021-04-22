097bdaa3650b78ce2c33ec7fe08a5db1e333bbb7
Narendra Santhosh Nagarajan <santhoshnarendra@gmail.com>
2021-04-22 15:24:35 +0530

    wip: connection of db with PHP completed

--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 // if, for, do while, while, for in
 // data types in php
 
@@ -7,9 +6,29 @@
 
 // var variable_name = "value"
 
-$name = $_POST['name'];
-$email=$_POST['email'];
-$message = $_POST['message'];
-echo "Hi ".$name."your mail id is ".$email."<br/> <b>".$message."</b>";
+
+// $name = $_POST['name'];
+// $email=$_POST['email'];
+// $message = $_POST['message'];
+// echo "Hi ".$name."your mail id is ".$email."<br/> <b>".$message."</b>";
+
+$serverName = "localhost";
+$databaseName = "batchsix";
+$userName = "root";
+$password = "";
+
+$connectionStatusQuery = new mysqli($serverName,$userName,$password,$databaseName);
+
+// var_dump($connectionStatusQuery);
+
+if($connectionStatusQuery->connect_error !== null){
+    echo "Unable to connect Database";
+}else{
+    echo "Connection was successful";
+}
+
+
+
+
 
 ?>
