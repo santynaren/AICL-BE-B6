dc97e548fac86d3f104fb81afb9d6b7a36678265
Narendra Santhosh Nagarajan <santhoshnarendra@gmail.com>
2021-05-05 15:42:35 +0530

    feat: get query param

--- index.php
+++ index.php
@@ -30,12 +30,13 @@ $connectionStatusQuery = new mysqli($serverName,$userName,$password,$databaseNam
 if($connectionStatusQuery->connect_error !== null){
     echo "Unable to connect Database";
 }else{
-
+$requestParam = $_GET['search'] ?? 0;
+echo $requestParam;
     $insertingFormData = "INSERT INTO $tableName VALUES ('','$name','$email','$number','$message')";
     $runQueryStatus = $connectionStatusQuery->query($insertingFormData);
    if($runQueryStatus){
         echo "Data inserted!";
-        $getDataFromDB = "SELECT * FROM $tableName";
+        $getDataFromDB = "SELECT * FROM $tableName LIMIT $requestParam";
         $runQueryGet = $connectionStatusQuery->query($getDataFromDB);
         if($runQueryGet->num_rows > 0){
             while($row = $runQueryGet->fetch_assoc()){
